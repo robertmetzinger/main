533222f49436706a5ee617246e3ad73bad9162de
patrick.woehnl <i16029@hb.dhbw-stuttgart.de>
2017-11-15 21:07:55 +0100

    Login erweitert Bug fix (Tastatut öffnet automatisch) BottomNavigationView bei start disabled ? funktioniert nicht

--- app/src/main/java/de/hb_dhbw_stuttgart/tutorscout24_android/LoginFragment.java
+++ app/src/main/java/de/hb_dhbw_stuttgart/tutorscout24_android/LoginFragment.java
@@ -3,10 +3,15 @@ package de.hb_dhbw_stuttgart.tutorscout24_android;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.design.internal.BottomNavigationItemView;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 
 /**
@@ -17,13 +22,11 @@ import android.view.ViewGroup;
  * Use the {@link LoginFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class LoginFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+public class LoginFragment extends android.app.Fragment {
+
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
 
@@ -63,8 +66,10 @@ public class LoginFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false);
+        View view =  inflater.inflate(R.layout.fragment_login, container, false);
+
+        ButterKnife.bind(this, view);
+        return  view;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
@@ -77,12 +82,7 @@ public class LoginFragment extends Fragment {
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
+
     }
 
     @Override
@@ -91,18 +91,17 @@ public class LoginFragment extends Fragment {
         mListener = null;
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
+
+
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
+    }
+
+    @OnClick(R.id.btnLogin)
+    public void  Login(){
+
+        BottomNavigationView nav = getActivity().findViewById(R.id.navigation);
+        nav.setEnabled(true);
     }
 }
--- app/src/main/java/de/hb_dhbw_stuttgart/tutorscout24_android/MainActivity.java
+++ app/src/main/java/de/hb_dhbw_stuttgart/tutorscout24_android/MainActivity.java
@@ -3,12 +3,15 @@ package de.hb_dhbw_stuttgart.tutorscout24_android;
 import android.annotation.SuppressLint;
 import android.app.Fragment;
 import android.app.FragmentTransaction;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.RequiresApi;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.MenuItem;
+import android.view.WindowManager;
 import android.widget.TextView;
 
 import butterknife.ButterKnife;
@@ -63,10 +66,12 @@ public class MainActivity extends AppCompatActivity {
 
                 case R.id.navigation_profile:
 
+
+
                     Log.e("nix", "onNavigationItemSelected: ");
                     Fragment profileFragment = new profileFragment();
                     transaction.replace(R.id.fragment2, profileFragment);
-                    transaction.addToBackStack(null);
+                    transaction.addToBackStack("Profil");
                     transaction.commit();
                     mTextMessage.setText(" ");
                     return true;
@@ -76,6 +81,7 @@ public class MainActivity extends AppCompatActivity {
 
     };
 
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -86,6 +92,20 @@ public class MainActivity extends AppCompatActivity {
         mTextMessage = findViewById(R.id.Home);
         BottomNavigationView navigation = findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
-    }
 
+        navigation.setEnabled(false);
+        navigation.setFocusable(false);
+        navigation.setFocusableInTouchMode(false);
+        navigation.setClickable(false);
+        navigation.setContextClickable(false);
+
+        FragmentTransaction transaction = getFragmentManager().beginTransaction();
+        LoginFragment loginFragment = new LoginFragment();
+        transaction.replace(R.id.fragment2, loginFragment);
+        transaction.addToBackStack("Login");
+        transaction.commit();
+
+        // Nötig da durch einen Fehler, vermutlich durch Focus einer EditText, autmoatisch sich die Tastatur beim Start öffnete.
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
+    }
 }
